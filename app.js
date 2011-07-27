043eefc86d075cf2d2f42ed2243828c7ca3610f9
Damon Oehlman <damon.oehlman@gmail.com>
2011-07-27 10:33:38 +1000

    Removed path override

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 module.exports = require('./lib/stack').configure({
     urls: [
         'http://localhost:8080/geoserver/wfs'
-    ],
-    datapath: '/development/projects/clients/racq/poidata/'
+    ]
 });
