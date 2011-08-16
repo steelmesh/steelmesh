ce1cac78441bba66d2b2722b94a2d1fc90bc5936
Damon Oehlman <damon.oehlman@gmail.com>
2011-08-16 16:38:18 +1000

    Updated packages

--- update-packages.sh
+++ update-packages.sh
@@ -1,11 +1,14 @@
 echo "Updating npm managed packages"
 npm update
 
+echo "Updating Comfy"
+npm rm comfy && npm install https://github.com/DamonOehlman/comfy/tarball/master
+
 echo "Updating GeoJS"
-npm install https://github.com/sidelab/geojs/tarball/master
+npm rm geojs && npm install https://github.com/sidelab/geojs/tarball/master
 
 echo "Updating quip"
-npm install https://github.com/caolan/quip/tarball/master
+npm rm quip && npm install https://github.com/caolan/quip/tarball/master
 
 echo "Removing .gitignore files"
 find node_modules -name .gitignore -exec rm {} \;
