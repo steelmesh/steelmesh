e2a9c019156273a3c95474607b705ae9469c1762
Damon Oehlman <damon.oehlman@gmail.com>
2011-12-06 23:08:10 +1000

    Updated core dashboard behaviour

--- assets/dashboard/public/js/dash.js
+++ assets/dashboard/public/js/dash.js
@@ -94,12 +94,127 @@ SteelmeshDash = (function() {
         });
     } // _changeLog
     
+    function _dashAction(targetUrl, callback) {
+        $.ajax({
+            url: targetUrl,
+            dataType: 'json',
+            success: function(data) {
+                (data.messages || []).forEach(function(message) {
+                    _showMessage(message);
+                });
+                
+                if (callback) {
+                    callback(data);
+                }
+            }
+        });
+    } // _dashAction
+    
+    function _deployPackage() {
+        var anchor = $(this),
+            targetUrl = '/deploy/' + anchor.data('package') + '?version=' + anchor.data('version'),
+            spinner;
+            
+        if (anchor.hasClass('disabled')) {
+            return;
+        } 
+        
+        // create the spinner
+        spinner = _spin('Deploying package');
+
+        $('.sm-packages a').addClass('disabled');
+        _dashAction(targetUrl, function(data) {
+            spinner.stop();
+            $('.sm-packages a').removeClass('disabled');
+        });
+    } // _deployPackage
+    
+    function _showMessage(msg, timeout) {
+        if (! msg) { return; }
+        
+        // add the message
+        var msgDiv = 
+            $('#status')
+                .append(
+                    '<div class="alert-message ' + (msg.type || '') + '" data-alert="true">' + 
+                    '<a class="close" href="#">×</a>' + 
+                    '<p>' + (msg.text || '') + '</p>' + 
+                    '</div>'
+                )
+                .find('.alert-message:last');
+                
+        setTimeout(function() {
+            msgDiv.fadeOut(function() {
+                msgDiv.remove();
+            });
+        }, timeout || 2000);
+    }
+    
+    function _spin(target, opts) {
+        opts = $.extend({
+          lines: 10, // The number of lines to draw
+          length: 3, // The length of each line
+          width: 2, // The line thickness
+          radius: 5, // The radius of the inner circle
+          color: '#000', // #rgb or #rrggbb
+          speed: 1.6, // Rounds per second
+          trail: 50, // Afterglow percentage
+          shadow: false // Whether to render a shadow
+        }, opts);
+        
+        if (typeof target == 'string') {
+            var msgDiv = $('#status')
+                    .append('<div class="sm-statustext"><span class="sm-spinner"></span><span>' + target + '</span></div>')
+                    .find('.sm-statustext:last'),
+                spinner = new Spinner(opts).spin(msgDiv.find('.sm-spinner')[0]);
+                
+            return {
+                stop: function() {
+                    spinner.stop();
+                    msgDiv.remove();
+                }
+            };
+        }
+        else {
+            return new Spinner(opts).spin(target);        
+        }
+        
+    }
+    
+    function _updateStatus() {
+        $.ajax({
+            url: '/status',
+            dataType: 'json',
+            success: function(data) {
+                var label = $('.sm-status .label'),
+                    status = data.status || 'unknown';
+                    
+                // reset the label classes
+                label[0].className = 'label';
+                
+                // update the text
+                label.html(status);
+                
+                // apply any extra styles
+                switch (status) {
+                    case 'online': {
+                        label.addClass('success');
+                        break;
+                    }
+                }
+            }
+        });
+    } // _updateStatus
+    
     $('a.log').click(function() {
         $('.pills li').removeClass('active');
         $(this).parent().addClass('active');
         
         _changeLog($(this).text());
     });
+    
+    $('.sm-packages a').click(_deployPackage);
+    setInterval(_updateStatus, 500);
     
     $().alert();
 })();
