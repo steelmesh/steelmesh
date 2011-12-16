455205a6cd4c74c7b30d3e202e00f09a7713e5e7
Damon Oehlman <damon.oehlman@gmail.com>
2011-12-16 19:57:32 +1000

    Reworking logging, fix monitor to better deal with couchdb outages

--- logs/exceptions/README.md
+++
@@ -1,1 +0,0 @@
-This is where steelmesh puts exception dumps
---
+++ test/logging.js
@@ -0,0 +1,33 @@
+var assert = require('assert'),
+    path = require('path'),
+    fs = require('fs'),
+    config = require('config'),
+    logPath = path.resolve(__dirname, '../logs'),
+    logger = require('../lib/helpers/logger'),
+    log = logger('test.log');
+    
+describe('logging', function() {
+    before(function(done) {
+        fs.unlink(path.join(logPath, 'test.log'), function(err) {
+            done();
+        });
+    });
+    
+    it('should support logging to different levels', function() {
+        assert(log.debug);
+        assert(log.info);
+        assert(log.warn);
+        assert(log.error);
+    });
+    
+    it('should be able to write a simple log line', function(done) {
+        log.info('test entry');
+        console.log(log.queue);
+
+        // wait for the log to flush
+        log.on('flush', function() {
+            // check that the file exists
+            fs.stat(path.join(logPath, 'test.log'), done);
+        });
+    });
+});
