5934c46a3fb764f144262933d7f0ed31c85a8f7d
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-02 10:54:00 +1000

    Start fleshing out server initialization

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-require('./lib/server')();
+var async = require('async');
+var config = require('rc')('steelmesh', {
+  server: 'http://localhost:5984/'
+});
+
+var nano = require('nano')(config.server);
+var appsync = require('steelmesh-appsync');
+
+function init(callback) {
+  async.parallel([
+    require('./init/couch')(nano, config)
+  ], callback);
+}
+
+function start(callback) {
+}
+
+async.series([init, start], function(err) {
+});
